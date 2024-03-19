49e1dedc4f3e43c3a19ae49440c38f7c9b20637a
4yusma <162754776+4yusma@users.noreply.github.com>
2024-03-20 02:09:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//variables
+var myname = "yusma";
+var sirname = "ahmed";
+console.log(myname + " " + sirname);
+var myage = "24";
+console.log(myage);
+var education = "intermediat";
+console.log(education);
+var cast = "ansari";
+console.log(cast);
+//HOBIES
+var hobby1 = "reading";
+var hobby2 = "shopping";
+var hobby3 = "planting";
+var hobby4 = "exploring new things";
+console.log(hobby1 + " " + hobby2 + " " + hobby3 + " " + hobby4);
+//goal
+console.log("being a programmer expert");
+//case sensitive
+//console.log("camel case");    
+//console.log("in camelcase, each word is capitalized so that it stands out from the rest of the next ");
+//addition
+var maths = 60;
+var english = 80;
+var socialStudies = 40;
+var urdu = 70;
+console.log(maths + english + socialStudies + urdu);
+var totalMarks = 100;
+var obtainMarks = 89;
+console.log(totalMarks - obtainMarks);
+var bluepens = 90;
+var blackpens = 88;
+console.log(bluepens * blackpens);
+var chocolate = 300;
+var students = 33;
+console.log(chocolate / students);
+//comparission operators
+var num8 = 4;
+var numo = 8;
+console.log(num8 == numo);
+var no = 90;
+var no1 = 80;
+console.log(no != no1);
---
+++ main.ts
@@ -0,0 +1,47 @@
+import { isEnumDeclaration } from "typescript";
+
+//variables
+let myname = `yusma`;
+let sirname =`ahmed`;
+console.log(myname+" "+sirname);
+let myage =`24`;
+console.log(myage);
+let education= `intermediat`;
+console.log(education);
+const cast =`ansari`;
+console.log(cast);
+//HOBIES
+let hobby1=`reading`;
+let hobby2=`shopping`;
+let hobby3=`planting`;
+let hobby4=`exploring new things`;
+console.log(hobby1 +" "+ hobby2 +" "+ hobby3 +" "+ hobby4);
+
+//goal
+console.log("being a programmer expert");
+
+//case sensitive
+//console.log("camel case");    
+//console.log("in camelcase, each word is capitalized so that it stands out from the rest of the next ");
+//addition
+let maths=60;
+let english=80;
+let socialStudies=40;
+let urdu=70;
+console.log(maths+english+socialStudies+urdu);
+let totalMarks=100;
+let obtainMarks=89;
+console.log(totalMarks-obtainMarks);
+const bluepens= 90;
+const blackpens=88;
+console.log(bluepens*blackpens);
+const chocolate=300;
+const students=33;
+console.log(chocolate/students);
+//comparission operators
+let num8 = 4;
+let numo = 8;
+console.log(num8==numo);
+let no=90;
+let no1= 80;
+console.log(no!=no1);
